491b07632a1d7b60e7f5ac431ecf6eab6e4fa74f
konichiwa55115 <121526988+konichiwa55115@users.noreply.github.com>
2023-03-04 00:30:54 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     plugins = dict(root="plugins")
     app = pyrogram.Client(
         "bot",
-        bot_token="5998058674:AAGHnCPi0MXLRGU3U4P9KbHeCCaVEbniFlk",
+        bot_token="5998058674:AAFFdL9RC7RWvyxDGVE6KxShEy1ZdZjUWSQ",
         api_id=int(17983098),
         api_hash="ee28199396e0925f1f44d945ac174f64",
         plugins=plugins,
